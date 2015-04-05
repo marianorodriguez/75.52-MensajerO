afca24836bc81288369a320ddfdfaa9635948382
Mariano Rodriguez <marianknot92@gmail.com>
2015-04-05 14:51:03 -0300

    agrego ServiceCollectionTest con 2 lineas comentadas para que se pueda compilar

---
+++ src/server/tests/services/ServiceCollectionTest.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+#include "ServiceCollectionTest.h"
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ServiceCollectionTest);
+
+void ServiceCollectionTest::setUp(){}
+
+void ServiceCollectionTest::tearDown(){}
+
+void ServiceCollectionTest::testAddService(){
+	CPPUNIT_FAIL("Estoy fallando :D");
+}
---
+++ src/server/tests/services/ServiceCollectionTest.h
@@ -0,0 +1,20 @@
+#ifndef SERVICECOLLECTIONTEST_H
+#define SERVICECOLLECTIONTEST_H
+
+#include "cppunit/extensions/HelperMacros.h"
+//#include "services/ServiceCollection.h"
+
+class ServiceCollectionTest : public CppUnit::TestFixture{
+	CPPUNIT_TEST_SUITE(ServiceCollectionTest);
+	CPPUNIT_TEST(testAddService);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+	void testAddService();
+private:
+//	ServiceCollection servCollection;
+};
+
+#endif // SERVICECOLLECTIONTEST_H
