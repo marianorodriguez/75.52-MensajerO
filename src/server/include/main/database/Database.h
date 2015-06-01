4b312bf79ccd36c797c7a9735a944b404efe6eef
Matías <matias@shishi>
2015-05-31 22:19:40 -0300

    Sin ticket - Corrijo gitignore

---
+++ src/server/include/main/database/Database.h
@@ -0,0 +1,69 @@
+/*
+ * Database.h
+ *
+ *  Created on: Mar 31, 2015
+ *      Author: fran
+ */
+
+#ifndef DATABASE_H_
+#define DATABASE_H_
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <rocksdb/db.h>
+#include "json/json.h"
+#include "IPersistible.h"
+#include "../exceptions/KeyNotFoundException.h"
+#include "../exceptions/InvalidKeyException.h"
+#include "../utilities/Logger.h"
+
+/*
+ * Esta clase define una base de datos donde la informacion se guarda como key-value.
+ */
+
+class Database: public IPersistible {
+private:
+	rocksdb::DB* database;
+public:
+	Database();
+	/*
+	 * Abre la base de datos en el @param path especificado. Si la base de datos no existe, la crea.
+	 */
+	Database(const string& path);
+
+	/*
+	 * Escribe informacion en la bd. Si @param key ya existe, el nuevo @param value reemplaza al anterior.
+	 * No importa el orden de los strings dentro del vector, los mismos strings siempre forman la misma key.
+	 */
+	void write(vector<string> key, const string& value) override;
+
+	/*
+	 * Retorna el value indicado por @param key. Si la key no existe, retorna @param error = true, si existe falso.
+	 */
+	string read(vector<string> key) const override;
+
+	/**
+	 * Devuelve un vector con todas las keys guardadas en la base de datos.
+	 */
+	std::vector<std::string> getAllKeys() const;
+
+	/*
+	 * Borra el @param key con el value asociado de la bd.
+	 */
+	void erase(vector<string> key);
+	/**
+	 * Cierra la base de datos
+	 */
+	void close();
+	virtual ~Database();
+
+private:
+	/*
+	 * Forma una key a partir de un vector de strings.
+	 */
+	string getKey(vector<string> key) const;
+	std::string pathDB;
+};
+
+#endif /* DATABASE_H_ */
---
+++ src/server/include/main/database/IPersistible.h
@@ -0,0 +1,35 @@
+/*
+ * IPersistible.h
+ *
+ *  Created on: Mar 31, 2015
+ *      Author: fran
+ */
+
+#ifndef IPERSISTIBLE_H_
+#define IPERSISTIBLE_H_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+ * Esta clase define una interfaz para objetos que puedan guardar informacion en archivos a partir de una key y un value.
+ */
+
+class IPersistible {
+public:
+	IPersistible();
+	virtual ~IPersistible();
+
+	/*
+	 * Escribe informacion a partir de @params key y value.
+	 */
+	virtual void write(vector<string> key, const string& value);
+	/*
+	 * Lee y retorna informacion en @param key.
+	 */
+	virtual string read(vector<string> key) const ;
+};
+
+#endif /* IPERSISTIBLE_H_ */
